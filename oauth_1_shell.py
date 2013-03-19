927f21b320eeb34f915c2c85126272aa6c2f82b4
Matt McClure <matthewlmcclure@gmail.com>
2013-03-19 15:55:09 -0400

    Reuse existing token credentials if possible
    Do a few quick tests to see if the token credentials in config.py look
    viable. If so, try to use them. If not, request new token credentials as
    before.

--- oauth_1_shell.py
+++ oauth_1_shell.py
@@ -11,56 +11,81 @@ from requests_oauthlib import OAuth1
 
 import config
 
-callback_uri='http://127.0.0.1:12345/'
+print
 
-oauth = OAuth1(config.client_credentials['oauth_token'][0],
-               config.client_credentials['oauth_token_secret'][0],
-               callback_uri=callback_uri,
-               signature_type=config.web_redirection_flow_signature_type)
-r = requests.post(url=config.temporary_credentials_url,
-                  headers={
-                      'Content-Type': 'application/x-www-form-urlencoded', 'Accept':
-                      'application/x-www-form-urlencoded'},
-                  auth=oauth)
-temporary_credentials = urlparse.parse_qs(r.content)
+try:
+    token_credentials = config.token_credentials
+    if not token_credentials['oauth_token'][0]:
+        raise Exception()
+    if not token_credentials['oauth_token_secret'][0]:
+        raise Exception()
+except:
+    print "'config.token_credentials' doesn't look usable."
+    print "Requesting new token credentials."
+    print
 
-authorize_url = '{0}?oauth_token={1}&oauth_callback={2}'.format(
-    config.user_authorization_url,
-    temporary_credentials['oauth_token'][0],
-    escape(callback_uri)
-)
+    callback_uri='http://127.0.0.1:12345/'
 
-webbrowser.open(authorize_url)
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-class AuthorizationHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200, 'OK')
-        self.send_header('Content-Type', 'text/html')
-        self.end_headers()
-        self.server.path = self.path
+    oauth = OAuth1(config.client_credentials['oauth_token'][0],
+                   config.client_credentials['oauth_token_secret'][0],
+                   callback_uri=callback_uri,
+                   signature_type=config.web_redirection_flow_signature_type)
+    r = requests.post(url=config.temporary_credentials_url,
+                      headers={
+                          'Content-Type': 'application/x-www-form-urlencoded', 'Accept':
+                          'application/x-www-form-urlencoded'},
+                      auth=oauth)
+    temporary_credentials = urlparse.parse_qs(r.content)
 
-server_address = (urlparse.urlparse(callback_uri).hostname,
-                  urlparse.urlparse(callback_uri).port)
-httpd = HTTPServer(server_address, AuthorizationHandler)
-httpd.handle_request()
-httpd.server_close()
-verifier_url = urlparse.urlparse(httpd.path)
-verifier_query = urlparse.parse_qs(verifier_url.query)
+    authorize_url = '{0}?oauth_token={1}&oauth_callback={2}'.format(
+        config.user_authorization_url,
+        temporary_credentials['oauth_token'][0],
+        escape(callback_uri)
+    )
 
-oauth = OAuth1(config.client_credentials['oauth_token'][0],
-               config.client_credentials['oauth_token_secret'][0],
-               unicode(temporary_credentials['oauth_token'][0]),
-               unicode(temporary_credentials['oauth_token_secret'][0]),
-               callback_uri=callback_uri,
-               signature_type=config.web_redirection_flow_signature_type,
-               verifier=unicode(verifier_query['oauth_verifier'][0]))
+    webbrowser.open(authorize_url)
+    from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+    class AuthorizationHandler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200, 'OK')
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.server.path = self.path
 
-r = requests.post(url=config.token_credentials_url,
-                  headers={
-                      'Content-Type': 'application/x-www-form-urlencoded',
-                      'Accept': 'application/x-www-form-urlencoded'},
-                  auth=oauth)
-token_credentials = urlparse.parse_qs(r.content)
+    server_address = (urlparse.urlparse(callback_uri).hostname,
+                      urlparse.urlparse(callback_uri).port)
+    httpd = HTTPServer(server_address, AuthorizationHandler)
+    httpd.handle_request()
+    httpd.server_close()
+    verifier_url = urlparse.urlparse(httpd.path)
+    verifier_query = urlparse.parse_qs(verifier_url.query)
+
+    oauth = OAuth1(config.client_credentials['oauth_token'][0],
+                   config.client_credentials['oauth_token_secret'][0],
+                   unicode(temporary_credentials['oauth_token'][0]),
+                   unicode(temporary_credentials['oauth_token_secret'][0]),
+                   callback_uri=callback_uri,
+                   signature_type=config.web_redirection_flow_signature_type,
+                   verifier=unicode(verifier_query['oauth_verifier'][0]))
+
+    r = requests.post(url=config.token_credentials_url,
+                      headers={
+                          'Content-Type': 'application/x-www-form-urlencoded',
+                          'Accept': 'application/x-www-form-urlencoded'},
+                      auth=oauth)
+    token_credentials = urlparse.parse_qs(r.content)
+else:
+    print "Using existing 'config.token_credentials'."
+    print
+    print "To request new token credentials, delete token_credentials from "
+    print "config.py and re-run."
+    print
+
+print "Display your token_credentials with:"
+print ">>> token_credentials"
+print
+print "Copy token_credentials to config.py to reuse them for future sessions."
+print
 
 oauth = OAuth1(config.client_credentials['oauth_token'][0],
                config.client_credentials['oauth_token_secret'][0],
@@ -68,7 +93,7 @@ oauth = OAuth1(config.client_credentials['oauth_token'][0],
                unicode(token_credentials['oauth_token_secret'][0]),
                signature_type=config.resource_signature_type)
 
-print "ready to make requests like:"
+print "Ready to make requests and display responses like:"
 print ">>> res = requests.get('http://example.com/some/resource', auth=oauth)"
 print ">>> res.json()"
 print ">>> print json.dumps(res.json(), indent=2)"
